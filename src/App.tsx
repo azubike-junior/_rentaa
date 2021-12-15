04dad84bc476acd55da6c96b2d5e7e633724e1e6
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-15 13:13:36 +0000

    Created the login page and the FormTitle component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Button from "./components/Button/index";
 import { HookInput } from "./components/BasicInputField";
+import LoginPage from "./pages/LoginPage/loginPage";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center px-24 max-w-3xl">
-      <HookInput show type="text" label="First Name" name="firstName" />
+    // <div className="flex flex-col justify-center px-24 max-w-3xl">
+    //   <HookInput show type="text" label="First Name" name="firstName" />
 
-      <Button
-        child="sign up"
-        type="submit"
-        className="w-full py-7 bg-secondary font-sans text-white rounded"
-      />
-    </div>
+    //   <Button
+    //     child="sign up"
+    //     type="submit"
+    //     className="w-full py-7 bg-secondary font-sans text-white rounded"
+    //   />
+    // </div>
+    <LoginPage />
   );
 }
 
--- src/pages/LoginPage/loginPage.tsx
+++ src/pages/LoginPage/loginPage.tsx
@@ -1,7 +1,21 @@
+import Button from "../../components/Button";
+import { HookInput } from './../../components/BasicInputField/index';
+import FormTitle from './../../components/FormTitle/index';
+
 export default function LoginPage() {
     return (
-        <div className="container mx-auto px-5">
-
+        <div className="container h-full flex flex-col justify-center mx-auto py-5 px-5 max-w-sm md:max-w-md lg:max-w-2xl">
+            <FormTitle title="Forgot your password?" instruction="Input your email address and check mail for password reset link"/>
+            <form>
+                <div className="formInputs mb-14 px-3 lg:px-0 lg:mb-20">
+                    <HookInput label="Email Address" name="email"/>
+                </div>
+                <Button
+                    child="Submit"
+                    type="submit"
+                    className="w-full py-7 bg-secondary font-sans text-white rounded lg:mx-3"
+                />
+            </form>
         </div>
     );
 }
