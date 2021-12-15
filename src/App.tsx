b9c0343df23a2a1fee514866d642052a5e2c605b
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-15 13:20:29 +0000

    Commented out the loginPage component

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import LoginPage from "./pages/LoginPage/loginPage";
 
 function App() {
   return (
-    // <div className="flex flex-col justify-center px-24 max-w-3xl">
-    //   <HookInput show type="text" label="First Name" name="firstName" />
+    <div className="flex flex-col justify-center px-24 max-w-3xl">
+      <HookInput show type="text" label="First Name" name="firstName" />
 
-    //   <Button
-    //     child="sign up"
-    //     type="submit"
-    //     className="w-full py-7 bg-secondary font-sans text-white rounded"
-    //   />
-    // </div>
-    <LoginPage />
+      <Button
+        child="sign up"
+        type="submit"
+        className="w-full py-7 bg-secondary font-sans text-white rounded"
+      />
+    </div>
+    // <LoginPage />
   );
 }
 
--- src/components/FormTitle/index.tsx
+++ src/components/FormTitle/index.tsx
@@ -6,8 +6,8 @@ type FormTitleProps = {
 export default function FormTitle({title, instruction} : FormTitleProps) {
     return (
         <div className="w-full mb-8 lg:mb-20 mx-auto px-3 lg:px-0">
-                <h1 className="text-2xl font-medium mb-3 lg:mb-6 md:text-3xl lg:text-5xl">{title}</h1>
-                <p className="text-sm md:text-md lg:text-lg">{instruction}</p>
-            </div>
+            <h1 className="text-2xl font-medium mb-3 lg:mb-6 md:text-3xl lg:text-5xl">{title}</h1>
+            <p className="text-sm md:text-md lg:text-lg">{instruction}</p>
+        </div>
     );
 }
