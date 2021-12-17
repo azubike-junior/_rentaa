a89737d4684963302ac3278cc86392746146f9a5
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-17 09:43:22 +0000

    Finished category styles, (still need to work on layout)

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -5,9 +5,9 @@ type CategoryProps = {
 
 export default function Category({imageUrl, categoryName} : CategoryProps){
     return (
-        <div className="w-36 md:w-48 mx-auto">
+        <div className="w-36 md:w-48 mb-6 md:mb-14 mr-4 md:mr-6 ">
             <img className="w-full rounded h-32 md:h-40 object-cover" src={imageUrl} alt="category"/>
-            <p className="text-center text-xs mt-1 md:text-sm">{categoryName}</p>
+            <p className="text-center text-xs mt-1 md:text-base">{categoryName}</p>
         </div>
     );
 }
