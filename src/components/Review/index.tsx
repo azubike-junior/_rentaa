d8b8df49408566b24636c737d799508a0d5c0019
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-21 10:12:50 +0000

    Finished the review section

---
+++ src/components/Review/index.tsx
@@ -0,0 +1,19 @@
+type ReviewProps = {
+    imageUrl : string;
+    reviewText : string;
+    authorName : string;
+}
+
+export default function Review({imageUrl, reviewText, authorName} : ReviewProps) {
+    return (
+        <div className="w-full px-3 md:px-8 lg:max-w-4xl flex flex-row my-5">
+            <div className="w-14 h-14 sm:w-20 sm:h-20 ">
+                <img src={imageUrl} className="w-14 h-14 sm:w-20 sm:h-20 object-cover rounded-full" />
+            </div>
+            <div className="ml-4 w-full">
+                <p className="text-sm sm:text-lg italic mb-1">{reviewText}</p>
+                <p className="text-xs sm:text-base">{authorName}</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/ReviewSection/index.tsx
@@ -0,0 +1,27 @@
+import Review from "../Review";
+
+export default function ReviewSection() {
+    return (
+        <div className="container md:max-w-7xl mx-auto">
+            <h1 className="text-2xl md:px-8 font-medium mt-5">Reviews</h1>
+            <Review 
+                imageUrl="https://st2.depositphotos.com/3758943/6040/i/950/depositphotos_60400959-stock-photo-a-break-at-work.jpg"
+                reviewText="'Working with Donald was very engaging. He was timely and made sure all our dealings were
+                very transparent'"
+                authorName="John Okafor"
+            />
+            <Review 
+                imageUrl="https://st2.depositphotos.com/3758943/6040/i/950/depositphotos_60400959-stock-photo-a-break-at-work.jpg"
+                reviewText="'Working with Donald was very engaging. He was timely and made sure all our dealings were
+                very transparent'"
+                authorName="John Okafor"
+            />
+            <Review 
+                imageUrl="https://st2.depositphotos.com/3758943/6040/i/950/depositphotos_60400959-stock-photo-a-break-at-work.jpg"
+                reviewText="'Working with Donald was very engaging. He was timely and made sure all our dealings were
+                very transparent'"
+                authorName="John Okafor"
+            />
+        </div>
+    );
+}
