ffd1a6c6435b5e59d375ba0e2cb47f75bd94027d
Azubuike Orji <Azubuike.Orji@Suntrustng.com>
2022-04-25 09:24:11 +0100

    chore: added link to the join button

--- src/components/AccessPossibilities/index.tsx
+++ src/components/AccessPossibilities/index.tsx
@@ -3,6 +3,7 @@ import iphone from '../../images/iphone.svg';
 import BottomLeft from '../../images/bottomLeft.png';
 import BottomRight from '../../images/bottomRight.png';
 import TopLeft from '../../images/topLeft.svg';
+import { Link } from 'react-router-dom';
 
 const AccessPossibilities : React.FC = () => {
     return (
@@ -18,7 +19,9 @@ const AccessPossibilities : React.FC = () => {
                 <p className="xs:w-80 xxs:w-60 xs:text-xs xxs:text-xs leading-6 text-sm md:text-base lg:text-xl text-center lg:text-left w-400 md:w-500 xl:w-600 mx-auto lg:mx-0  font-normal mt-4 lg:mt-10">We’re building <span className="font-bold">faster</span> and <span className='font-bold'>stress-free</span>  ways to rent gadgets from, and to anyone without any hassle. Giving you complete <span className="font-bold">access</span> to work better, and earn better.</p>
                 <div className='mt-6 lg:mt-16 text-center lg:text-left'>
                     <button data-aos="zoom-in" className='xs:text-xs xxs:text-xs text-sm md:text-base lg:text-lg py-3 lg:py-4 px-5 lg:px-7 bg-secondary text-white rounded-md mr-5'>Our story</button>
+                    <Link to="/login">
                     <button data-aos="zoom-in" className='xs:text-xs xxs:text-xs text-sm md:text-base lg:text-lg py-3 lg:py-4.5 px-5 lg:px-7 text-secondary border-2 rounded-md border-secondary'>Join Private Beta</button>
+                    </Link>
                 </div>
             {/* Set the line hieght of the second paragraph to 18px */}
             {/* Remember to change the line height of the first paragraph to 65.1px */}
