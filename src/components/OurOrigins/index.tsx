874c4a88d913e5920b9c99cecad72ad8788e1e37
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-25 22:57:20 +0100

    Finished working on the ourOrigins section

---
+++ src/components/OurOrigins/index.tsx
@@ -0,0 +1,10 @@
+const OurOrigins : React.FC = () => {
+    return (
+        <div className="flex flex-col items-center gap-6 px-8">
+            <h2 className="font-dosis text-2xl lg:text-5xl font-bold">Our Origins</h2>
+            <p className="font-dm-sans text-xs lg:text-xl font-normal text-center max-w-6xl">We started in September 2021 with one goal in mind; to disrupt the p2p gadget lending space in Nigeria. Our work since then has been a testament to that fact.</p>
+            <p className="font-dm-sans text-xs lg:text-xl font-normal text-center max-w-6xl">Having identified a problem of e-waste management in Nigeria as well as rising costs of gadgets, we decided to solve that problem by creating a platform with our users at the centre of it. Our sole focus is in how best we can serve this ripe market in Nigeria and make a world of difference in the process.</p>
+        </div>
+    )
+}
+export default OurOrigins;
--- src/pages/OurStory/index.tsx
+++ src/pages/OurStory/index.tsx
@@ -2,7 +2,7 @@
 const OurStory : React.FC = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
