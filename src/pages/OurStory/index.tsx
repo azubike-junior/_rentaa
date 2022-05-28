8b9ae831ce4260105b74a935bdb34f2efb7b0300
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-28 14:02:29 +0100

    Finished working on the OurStory section

--- src/pages/OurStory/index.tsx
+++ src/pages/OurStory/index.tsx
@@ -1,8 +1,19 @@
+import ImagePanel from "../../components/ImagePanel";
+import LandingPageNavBar from "../../components/LandingPageNavBar";
+import Newsletter from "../../components/Newsletter";
+import OurMission from "../../components/OurMission";
+import OurOrigins from "../../components/OurOrigins";
+import WhatWeOffer from "../../components/WhatWeOffer";
 
 const OurStory : React.FC = () => {
     return (
-        <div>
-
+        <div className="pt-12">
+            <LandingPageNavBar />
+            <OurMission />
+            <OurOrigins />
+            <ImagePanel />
+            <WhatWeOffer />
+            <Newsletter />
         </div>
     )
 }
