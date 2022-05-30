5120c59b92ba0441b42c428953b6e2c99414f1ef
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-30 07:56:34 +0100

    Finished working on the settings section

--- src/components/SettingsSection/index.tsx
+++ src/components/SettingsSection/index.tsx
@@ -1,12 +1,16 @@
 import SettingsIcon from '../../images/settingsIcon.svg';
 const SettingsSection = () => {
+    //TODO: Make the following styles more accurate with tailwind config: 
+    // Box shadow
+    // Border radius
+    // Left Padding of the list
     return (
-        <section className="w-full max-w- px-7">
-            <div className='inline-flex gap-3'>
+        <section className="w-full max-w-xxxs px-7 py-11 shadow-xl rounded-3xl">
+            <div className='inline-flex gap-3 mb-5'>
                 <img src={SettingsIcon} />
                 <p className='text-lg text-secondary'>Settings</p>
             </div>
-            <ul className='flex flex-col gap-4'>
+            <ul className='flex flex-col gap-4 pl-9'>
                 <li className='text-lightGrey'>Edit profile</li>
                 <li className='text-lightGrey'>Change Password</li>
                 <li className='text-lightRed'>Log out</li>
