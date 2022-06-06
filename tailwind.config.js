797bd5eec3ccb70dcf85810a6b79ceb960c47f92
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-06 16:21:57 +0100

    Added a custom property to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -84,7 +84,8 @@ module.exports = {
         darkBlue: "#150C38",
         lightRed: "rgba(212, 13, 13, 0.49)",
         grey: "#D3D3D3", 
-        mediumGrey: "#606060"
+        mediumGrey: "#606060", 
+        transparentBlack: "rgba(0, 0, 0, 0.5)",
       },
       boxShadow: {
         'xmd': '0px 4px 40px 0px rgba(0, 0, 0, 0.15)'
@@ -94,6 +95,7 @@ module.exports = {
         'profileLayoutM': 'auto 278px', 
       },
       borderRadius: {
+        100: '100px', 
         20: '20px'
       }, 
       inset: {
