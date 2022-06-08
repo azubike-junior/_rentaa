b9e3e0f61eb063791b37187ed055bc390d6814ed
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-08 17:53:30 +0100

    Successfully added the edit profile modal to the page

--- src/components/SettingsSection/index.tsx
+++ src/components/SettingsSection/index.tsx
@@ -1,11 +1,12 @@
 import SettingsIcon from '../../images/settingsIcon.svg';
 import { Dispatch, SetStateAction } from 'react';
+import { toggleReviewModal } from "../../services/Mutations/Modal";
+import { useDispatch } from 'react-redux';
+import { toggleEditModal } from "../../services/Mutations/Modal";
 
-interface SettingsSectionProps {
-    setEditProfileModalIsShowing : Dispatch<SetStateAction<boolean>>;
-}
 
-const SettingsSection : React.FC<SettingsSectionProps>  = ({setEditProfileModalIsShowing}) => {
+const SettingsSection : React.FC = () => {
+    const dispatch = useDispatch();
     return (
         <section className="w-full max-w-278px xl:max-w-xxxs px-7 py-11 shadow-xmd rounded-20">
             <div className='inline-flex gap-3 mb-5'>
@@ -13,7 +14,7 @@ const SettingsSection : React.FC<SettingsSectionProps>  = ({setEditProfileModalI
                 <p className='text-lg text-secondary'>Settings</p>
             </div>
             <ul className='flex flex-col gap-4 pl-9'>
-                <li className='text-lightGrey cursor-pointer' onClick={() => setEditProfileModalIsShowing(true)}>Edit profile</li>
+                <li className='text-lightGrey cursor-pointer' onClick={() => dispatch(toggleEditModal())}>Edit profile</li>
                 <li className='text-lightGrey cursor-pointer'>Change Password</li>
                 <li className='text-lightRed cursor-pointer'>Log out</li>
             </ul>
--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -4,22 +4,27 @@ import ReviewsSection from "../../components/ReviewsSection";
 import SettingsSection from "../../components/SettingsSection";
 import UserProfileSection from "../../components/UserProfileSection";
 import Modal from "../../components/Modal";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const ProfilePage = () => {
-    const [EditProfileModalIsShowing, setEditProfileModalIsShowing] = useState(false);
+    const { editModalOpen } = useSelector(
+        (state: RootState) => state.modalReducer
+      );
+    
     return (
     <>
-        <div className="mx-auto container w-full pt-4 px-8 grid grid-cols-profileLayoutM xl:grid-cols-profileLayoutL gap-5">
+        <div className="mx-auto container md:max-w-screen-lg xl:max-w-screen-xl w-full pt-4 px-8 grid grid-cols-profileLayoutM xl:grid-cols-profileLayoutL gap-5">
             <section>
                 <UserProfileSection />
             </section>
             <section className="flex flex-col gap-9">       
-                <SettingsSection setEditProfileModalIsShowing={setEditProfileModalIsShowing}/>
+                <SettingsSection />
                 <ReviewsSection />
             </section>
         </div>
             {
-                <Modal isOpen={EditProfileModalIsShowing}>
+                <Modal isOpen={editModalOpen}>
                         <EditProfileModal/>
                 </Modal>
             }
