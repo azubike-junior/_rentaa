1c2ef3296fd0de16e17cdb98e16024a6a323c625
mac <mac@macs-MacBook-Pro.local>
2022-12-21 22:29:30 +0100

    chore: changed content on FAQs page

--- src/utils/accordion.ts
+++ src/utils/accordion.ts
@@ -1,26 +1,33 @@
 export const accordionData = [{
-  title: "Can I rent more than one gaget at once? ",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac placerat aenean turpis id consectetur tincidunt. Feugiat velit eu platea sem leo. Justo, fermentum, sit vestibulum eget ipsum, augue ultrices. Nulla convallis senectus sed orci feugiat volutpat nibh faucibus porttitor."
+  title: "Is there a deadline to make payment for my order ",
+  content: "A: Yes, the payment link shared with you expires after 24hrs."
 },
   
   {
-  title: "How can I protect my gadeget from damage and theft ",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac placerat aenean turpis id consectetur tincidunt. Feugiat velit eu platea sem leo. Justo, fermentum, sit vestibulum eget ipsum, augue ultrices. Nulla convallis senectus sed orci feugiat volutpat nibh faucibus porttitor."
+  title: "Am I responsible for anything that happens to the items while it is with me? ",
+  content: "A: Yes, you are responsible for any damages. However you can sign up for our insurance cover and be protected from any damages incurred."
   },
   
   {
-  title: "How can I protect my gadeget from damage and theft",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac placerat aenean turpis id consectetur tincidunt. Feugiat velit eu platea sem leo. Justo, fermentum, sit vestibulum eget ipsum, augue ultrices. Nulla convallis senectus sed orci feugiat volutpat nibh faucibus porttitor."
+  title: "Is there any warranty on the items?",
+  content: "A: If the item(s) you rented are not what you expect, please contact support for next steps."
 },
 
   {
-  title: "How can I protect my gadeget from damage and theft ",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac placerat aenean turpis id consectetur tincidunt. Feugiat velit eu platea sem leo. Justo, fermentum, sit vestibulum eget ipsum, augue ultrices. Nulla convallis senectus sed orci feugiat volutpat nibh faucibus porttitor."
+  title: " Who determines the rent fees for my property?",
+  content: "A: The price(s) of your listing(s) are totally up to you. However, we'd recommend you list with a competitive price to attract more Renters."
   },
   
   {
-  title: "Can I rent more than one gaget at once? ",
-  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac placerat aenean turpis id consectetur tincidunt. Feugiat velit eu platea sem leo. Justo, fermentum, sit vestibulum eget ipsum, augue ultrices. Nulla convallis senectus sed orci feugiat volutpat nibh faucibus porttitor."
+  title: "How do I receive my order?",
+    content: "A: Your rental order(s) will be delivered to your desired location within a short time."
+  
+  },
+  
+    {
+  title: "Will I order for logistics to deliver my order?",
+    content: "A: No, Rentout will deliver your order to you with their logistics service."
+  
 },
 
 ]
